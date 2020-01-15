ca2392efcd551e4ee666138b0f4596f903ac6da8
jmeade11 <hobokenjam@yahoo.com>
2020-01-15 04:51:41 -0500

    Update for SEIR-129
    1. Updated react-scripts from 3.0.0 to 3.3.0
    2. Updated prettier config
    3. Removed unneeded files
    4. Removed miscellaneous CSS declarations in index.css

--- src/App.js
+++ src/App.js
@@ -1,65 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import ErrorPage from './ErrorPage';
 
 export default class App extends Component {
   constructor() {
-    super()
-    
+    super();
+
     this.state = {
       page: 'home'
-    }
-
-    this.setPageToHome = this.setPageToHome.bind(this)
-    this.setPageToAbout = this.setPageToAbout.bind(this)
+    };
   }
 
-  setPageToHome(e) {
-    e.preventDefault()
+  setPageToHome(event) {
+    event.preventDefault();
     this.setState({
-      page: 'home',
-    })
+      page: 'home'
+    });
   }
 
-  setPageToAbout(e) {
-    e.preventDefault()
+  setPageToAbout(event) {
+    event.preventDefault();
     this.setState({
-      page: 'about',
-    })
+      page: 'about'
+    });
   }
   render() {
-    let Page
-    let pageTitle
+    let Page;
+    let pageTitle;
 
     switch (this.state.page) {
       case 'home':
-        Page = HomePage
-        pageTitle = "Home"
+        Page = HomePage;
+        pageTitle = 'Home';
         break;
       case 'about':
-        Page = AboutPage
-        pageTitle = "About"
+        Page = AboutPage;
+        pageTitle = 'About';
         break;
       default:
-        Page = ErrorPage
-        pageTitle = "Error"
+        Page = ErrorPage;
+        pageTitle = 'Error';
         break;
     }
-    
+
     return (
       <div className="page-container">
         <header>
           <h1>Component Lifecycle</h1>
           <nav>
-            <a href="/" onClick={this.setPageToHome}>Home</a>
-            <a href="/about" onClick={this.setPageToAbout}>About</a>
+            <a href="/" onClick={this.setPageToHome}>
+              Home
+            </a>
+            <a href="/about" onClick={this.setPageToAbout}>
+              About
+            </a>
           </nav>
         </header>
         <main>
           <Page message={pageTitle}></Page>
         </main>
       </div>
-    )
+    );
   }
 }
